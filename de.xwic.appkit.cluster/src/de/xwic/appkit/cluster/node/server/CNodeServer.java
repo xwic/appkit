d102cf16c78b287ad2d5cb9e6af28b695271ac6e
Florian Lippisch <lippisch@netapp.com>
2012-07-07 15:11:37 +0200

    Initial Commit

---
+++ de.xwic.appkit.cluster/src/de/xwic/appkit/cluster/node/server/CNodeServer.java
@@ -0,0 +1,400 @@
+/**
+ * 
+ */
+package de.xwic.appkit.cluster.node.server;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.StringTokenizer;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.mortbay.jetty.Handler;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.ContextHandlerCollection;
+import org.mortbay.jetty.webapp.WebAppContext;
+import org.mortbay.xml.XmlConfiguration;
+
+/**
+ * @author Developer
+ *
+ */
+public class CNodeServer implements Runnable {
+	
+	private static CNodeServer instance = null;
+	
+	private static final Log log = LogFactory.getLog(CNodeServer.class);
+	
+	private static final long SLEEP_TIME = 5 * 1000; // 5 sec.
+	
+	private String rootPath = ".";
+	private Server jetty;
+	
+	private ServerContext serverContext = new ServerContext();
+	
+	private boolean initialized = false;
+	private boolean initializing = false;
+	private boolean doExit = false;
+	private boolean running = false;
+	private boolean exitAfterFinish = false;
+	
+	public static boolean FORCE_LOG4J_INITIALIZATION = false;
+
+	/**
+	 * 
+	 */
+	public CNodeServer() {
+		instance = this;
+	}
+	
+	/**
+	 * Returns the server instance.
+	 * @return
+	 */
+	public static CNodeServer getInstance() {
+		if (instance == null) {
+			instance = new CNodeServer();
+		}
+		return instance;
+	}
+
+	/**
+	 * Exit the server.
+	 */
+	public void exitServer() {
+		doExit = true;
+	}
+	
+	/**
+	 * Startup the server.
+	 */
+	public void run() {
+
+		log.info("Server started.");
+		
+		running = true;
+		
+		while (!doExit) {
+			
+			try {
+				Thread.sleep(SLEEP_TIME);
+			} catch (InterruptedException e) {
+				// nothing unexpected.
+			}
+		}
+		
+		try {
+			if (jetty != null) {
+				jetty.stop();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		running = false;
+		
+	}
+	
+
+	/**
+	 * Initialize the server.
+	 * @throws ETLException 
+	 */
+	public boolean initialize() {
+		if (initialized) {
+			System.out.println("Server is already initialized.");
+			return true;
+		}
+		try {
+			initializing = true;
+			initialized = initializeServer();
+			return initialized;
+		} finally {
+			initializing = false;
+		}
+	}
+	
+	/**
+	 * Initialize the server.
+	 * @return
+	 */
+	private boolean initializeServer() {
+		
+		File path = new File(rootPath);
+		if (!path.exists()) {
+			System.out.println("Root path " + path.getAbsolutePath() + " does not exist.");
+			return false;
+		}
+		
+		// set root path of etlgine in system enrionment
+		System.setProperty("etlgine_rootPath", path.getAbsolutePath());
+		
+		// set root path in server context
+		serverContext.setProperty(ServerContext.PROPERTY_ROOTPATH, path.getAbsolutePath());
+		
+		File pathConfig = new File(path, "config");
+		if (!pathConfig.exists()) {
+			System.out.println("Config path " + pathConfig.getAbsolutePath() + " does not exist.");
+			return false;
+		}
+		//log.info("Redirecting System.out");
+		//System.setOut(new PrintStream(new ScreenOutputStream(screen)));
+
+		// Initialize logging (only if it had not yet been initialized)
+		if (FORCE_LOG4J_INITIALIZATION || !Logger.getRootLogger().getAllAppenders().hasMoreElements()) {
+
+			File configFile = new File(pathConfig, "log4j.xml");
+			boolean isXml = configFile.exists(); 
+			if (!isXml) {
+				configFile = new File(pathConfig, "log4j.properties");
+				if (!configFile.exists()) {
+					System.out.println("Log4J config file log4j.xml or log4j.properties does not exist.");
+					return false;
+				}
+			}
+
+			System.out.println("Initializing Log4J");
+			if (isXml) {
+				DOMConfigurator.configureAndWatch(configFile.getAbsolutePath(), 2000);
+			} else {
+				PropertyConfigurator.configureAndWatch(configFile.getAbsolutePath(), 2000);
+			}
+		}
+		
+		File fileServerConf = new File(pathConfig, "server.properties");
+		if (!fileServerConf.exists()) {
+			log.error("server.properties not found.");
+			return false;
+		}
+
+		Properties props = new Properties();
+		try {
+			props.load(new FileInputStream(fileServerConf));
+		} catch (IOException e) {
+			log.error("log.error reading server.properties: " + e);
+			return false;
+		}
+		
+		// search for an "override" file
+		File fileServerConfOVR = new File(pathConfig, "server.override.properties");
+		if (fileServerConfOVR.exists()) {
+			Properties props2 = new Properties();
+			try {
+				props2.load(new FileInputStream(fileServerConfOVR));
+			} catch (IOException e) {
+				log.error("log.error reading server.override.properties: " + e);
+				return false;
+			}
+			// merge properties
+			for (Enumeration<Object> e = props2.keys(); e.hasMoreElements(); ) {
+				String key = (String)e.nextElement();
+				String value = props2.getProperty(key);
+				props.setProperty(key, value);
+			}
+		}
+
+		
+		// copy properties to server context
+		for (Object key : props.keySet()) {
+			String sKey = (String)key;
+			if (serverContext.getProperty(sKey) == null) {
+				// set only new properties
+				serverContext.setProperty(sKey, props.getProperty(sKey));
+			}
+		}
+
+		// load jobs
+		File jobPath = new File(path, serverContext.getProperty(ServerContext.PROPERTY_SCRIPTPATH, "jobs"));
+		if (!jobPath.exists()) {
+			log.error("The job directory does not exist: " + jobPath.getAbsolutePath());
+			return false;
+		}
+		serverContext.setProperty(ServerContext.PROPERTY_SCRIPTPATH, jobPath.getAbsolutePath());
+		
+		StringTokenizer stk = new StringTokenizer(serverContext.getProperty("loadJobs", ""), ",; ");
+		while (stk.hasMoreTokens()) {
+			String scriptName = stk.nextToken();
+			loadJob(scriptName);
+		}
+
+		// load DataPool(s)
+		CubeHandler cubeHandler = CubeHandler.getCubeHandler(serverContext);
+		log.info("Loaded " + cubeHandler.getDataPoolManagerKeys().size() + " DataPool(s).");
+		
+		// load webserver
+		if (serverContext.getPropertyBoolean(ServerContext.PROPERTY_WEBSERVER_START, false)) {
+			log.info("Initializing Webserver (Jetty)");
+			try {
+				jetty = new Server();
+				/*
+				 * If ETLgine is launched from a different "current directory",
+				 * this is the the only way to set jetty home directoy.
+				 * Might have side effects on the "hosting" application.
+				 */
+				/*
+				String userDir = System.getProperty("user.dir");
+				String newUserDir = new File(rootPath).getCanonicalPath();
+				if (!newUserDir.startsWith(userDir)) {
+					// etl server is located in different path
+					// workaround for now is to set user.dir to newUserDir
+					// FIX-ME do it better ;-)
+					System.setProperty("user.dir", newUserDir);
+				}
+				
+				2009-10-13 jbornema
+					- fixed in using jetty <SystemProperty name="etlgine_rootPath" default="."/> in xml files
+				*/
+				XmlConfiguration conf = new XmlConfiguration(new FileInputStream(new File(pathConfig, "jetty.xml")));
+				conf.configure(jetty);
+	
+				Handler[] handlers = jetty.getHandlers();
+			    ContextHandlerCollection context = null;
+			    for (Handler h : handlers) {
+			    	if (h instanceof ContextHandlerCollection) {
+			    		context = (ContextHandlerCollection)h;
+			    		break;
+			    	}
+			    }
+			    if (context == null) {
+			    	log.error("Invalid Jetty Configuration - no ContextHandlerCollection found.");
+			    	return false;
+			    }
+				WebAppContext wc = new WebAppContext(context, new File(path, "web").getAbsolutePath(), "/etlgine");
+				wc.setDefaultsDescriptor(new File(pathConfig, "webdefault.xml").getAbsolutePath());
+				
+				jetty.start();
+				
+			} catch (Exception e) {
+				log.error("Error starting webserver: " + e, e);
+			}
+		}
+		
+		// execute run scripts, if specified
+		
+		stk = new StringTokenizer(serverContext.getProperty("run", ""), ",; ");
+		while (stk.hasMoreTokens()) {
+			String scriptName = stk.nextToken();
+			try {
+				executeStartScript(scriptName);
+			} catch (Exception e) {
+				log.error("Error running startscript: " + e, e);
+				return false;
+			}
+		}
+		
+		if (serverContext.getPropertyBoolean("notifications.enabled", false)) {
+			log.info("Notification Services enabled");
+			NotificationService nfService = new NotificationService(serverContext);
+			serverContext.addServerContextListener(nfService);
+		}
+		
+		//System.setOut(oldPS);
+		return true;
+		
+	}
+
+	/**
+	 * @param scriptName
+	 * @throws Exception 
+	 */
+	private void executeStartScript(String scriptName) throws Exception {
+		
+		log.info("Executing Startscript " + scriptName);
+		
+		Binding binding = new Binding();
+		binding.setVariable("context", serverContext);
+
+		GroovyShell shell = new GroovyShell(binding);
+		
+		File file = new File(new File(rootPath), scriptName);
+		if (!file.exists()) {
+			throw new ETLException("The script file " + file.getAbsolutePath() + " does not exist.");
+		}
+		
+		try {
+			shell.evaluate(file);
+		} catch (Exception e) {
+			throw new ETLException("Error evaluating script '" + file.getName() + "':" + e, e);
+		}
+
+		
+	}
+
+	/**
+	 * @return the rootPath
+	 */
+	public String getRootPath() {
+		return rootPath;
+	}
+
+	/**
+	 * @param rootPath the rootPath to set
+	 */
+	public void setRootPath(String rootPath) {
+		this.rootPath = rootPath;
+	}
+
+	/**
+	 * @return the serverContext
+	 */
+	public ServerContext getServerContext() {
+		return serverContext;
+	}
+
+	/**
+	 * @return the exitAfterFinish
+	 */
+	public boolean isExitAfterFinish() {
+		return exitAfterFinish;
+	}
+
+	/**
+	 * @param exitAfterFinish the exitAfterFinish to set
+	 */
+	public void setExitAfterFinish(boolean exitAfterFinish) {
+		this.exitAfterFinish = exitAfterFinish;
+	}
+
+	/**
+	 * @return the initialized
+	 */
+	public boolean isInitialized() {
+		return initialized;
+	}
+	
+	/**
+	 * @return the initializing
+	 */
+	public boolean isInitializing() {
+		return initializing;
+	}
+
+	public boolean isRunning() {
+		return running;
+	}
+
+	/**
+	 * Loads a job from groovy script and returns it.
+	 * @param scriptName
+	 * @return
+	 */
+	public IJob loadJob(String scriptName) {
+		String jobName = scriptName;
+		if (jobName.toLowerCase().endsWith(".groovy")) {
+			jobName = jobName.substring(0, jobName.length() - ".groovy".length());
+		}
+		try {
+			log.info("Loading Job " + jobName + " from file " + scriptName);
+			return serverContext.loadJob(jobName, scriptName);
+		} catch (Throwable e) {
+			log.error("An error occured during loading of the job " + scriptName + ": " + e, e);
+		}
+		return null;
+	}
+}
---
+++ de.xwic.appkit.cluster/src/de/xwic/appkit/cluster/node/start/Launch.java
@@ -0,0 +1,159 @@
+/**
+ * 
+ */
+package de.xwic.appkit.cluster.node.start;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import de.xwic.appkit.cluster.node.server.CNodeServer;
+
+/**
+ * Start the node as a stand-alone node. This will bring up the internally used
+ * Jetty Server, to allow access to the node and allow HTTP based communication.
+ * 
+ * @author lippisch
+ *
+ */
+public class Launch {
+
+	private final static String PARAM_PATH = "path";
+	private final static String PARAM_CONSOLE = "console";
+	private final static String PARAM_RUN = "run";
+	private final static String PARAM_HELP = "help";
+	
+	private static Map<String, String> PARAMS = new HashMap<String, String>();
+	static {
+		PARAMS.put(PARAM_PATH, PARAM_PATH);
+		PARAMS.put("p", PARAM_PATH);
+		PARAMS.put(PARAM_CONSOLE, PARAM_CONSOLE);
+		PARAMS.put("con", PARAM_CONSOLE);
+		PARAMS.put(PARAM_RUN, PARAM_RUN);
+		PARAMS.put("r", PARAM_RUN);
+		PARAMS.put("h", PARAM_HELP);
+		PARAMS.put(PARAM_HELP, PARAM_HELP);
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// force log4j initialization
+		CNodeServer.FORCE_LOG4J_INITIALIZATION = true;
+		
+		System.out.println("XWic - CNode Launcher");
+		System.out.println("---------------------");
+		System.out.println();
+		
+		// parse arguments
+		Properties prop;
+		try {
+			prop = getLaunchArgs(args);
+		} catch (IllegalArgumentException e) {
+			System.out.println("Invalid arguments specified: " + e.getMessage());
+			return;
+		}
+		
+		if (prop.getProperty(PARAM_HELP, null) != null) {
+			System.out.println("Arguments: [-help][-path <base path>] [-run <job name>] [-console [swing]]");
+			return;
+		}
+		
+		String path = prop.getProperty(PARAM_PATH, ".");
+		String rootPath = new File(path).getAbsolutePath();
+		try {
+			rootPath = new File(path).getCanonicalPath();
+		} catch (IOException ioe) {}
+		
+		System.setProperty("root", rootPath);
+		CNodeServer server = CNodeServer.getInstance();
+		server.setRootPath(rootPath);
+		if (!server.initialize()) {
+			System.out.println("Server start failed.");
+			return;
+		}
+		
+
+		String runJob = prop.getProperty(PARAM_RUN);
+
+		// startup the server.
+		Thread serverThread = new Thread(server, "ETLgineServer");
+		serverThread.setDaemon(false);
+		serverThread.start();
+		if (prop.getProperty(PARAM_CONSOLE) != null) {
+			ETLgineConsole console = new ETLgineConsole(server);
+			if ("swing".equals(prop.getProperty(PARAM_CONSOLE))) {
+				SwingRunner.launch(console, true);
+			} else {
+				SwtRunner.launch(console, true);
+			}
+		}
+		
+		if (runJob != null) {
+			for (String rJob : runJob.split("[;,]")) {
+				IJob job = server.getServerContext().getJob(rJob);
+				if (job == null) {
+					job = server.loadJob(rJob + ".groovy");
+					//System.out.println("The specified job does not exist: " + runJob);
+				}
+				if (job != null) {
+					server.enqueueJob(job);
+				}
+			}
+			server.setExitAfterFinish(true);
+		}
+
+	}
+
+	/**
+	 * @param args
+	 * @return
+	 */
+	private static Properties getLaunchArgs(String[] args) {
+		
+		Properties prop = new Properties();
+		
+		String cmd = null;
+		for (int i = 0 ; i < args.length; i++) {
+			
+			if (args[i].startsWith("-")) {
+				// is command
+				if (cmd != null && prop.getProperty(cmd, null) == null) {
+					prop.setProperty(cmd, "1");
+				}
+				if (args[i].length() <= 1) {
+					throw new IllegalArgumentException("The argument #" + i + " is a '-' only, which is not allowed.");
+				}
+				String key = args[i].substring(1);
+				if (!PARAMS.containsKey(key)) {
+					throw new IllegalArgumentException("Unknown argument: " + key);
+				}
+				cmd = PARAMS.get(key);
+			} else {
+				if (cmd == null) {
+					throw new IllegalArgumentException("No key value specified for argument #" + i + ". Enter -help for more infomrations.");
+				}
+				String old = prop.getProperty(cmd, null);
+				if (old != null) {
+					prop.setProperty(cmd, old + " " + args[i]);
+				} else {
+					prop.setProperty(cmd, args[i]);
+				}
+			}
+		}
+		if (cmd != null) {
+			String old = prop.getProperty(cmd, null);
+			if (old == null) {
+				prop.setProperty(cmd, "1");
+			}
+		}
+		
+		return prop;
+	}
+
+
+}
