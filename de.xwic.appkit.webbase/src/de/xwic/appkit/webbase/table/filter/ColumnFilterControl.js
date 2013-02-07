798ae4b6e777395daf79c302c690759ff8b96690
bogdanp <bogdanp@netapp.com>
2013-02-07 18:28:27 +0200

    ported appkit ColumnFilter and Column Action to jQuery

--- de.xwic.appkit.webbase/src/de/xwic/appkit/webbase/table/filter/ColumnFilterControl.js
+++ de.xwic.appkit.webbase/src/de/xwic/appkit/webbase/table/filter/ColumnFilterControl.js
@@ -1,93 +1,60 @@
 {
-	beforeUpdate: function() {},
-	doUpdate: function(element) {
+		
+	doUpdate: function(){		
 		return false;
 	},
+	
 	/**
 	 * Initialize the window if it does not already exist.
 	 */
 	afterUpdate: function(element) {
-	#if($control.visible)
-		var win = Windows.getWindow("$control.controlID");
-		if (win && (win._positionIndex != $control.positionIdx)) {
-			win.hide();
-			win.destroy();
-			win = null;
-		}
-		if (!win) {
-			// open the window
-			
-			// find the column
-			var myWidth = 300;
-			var top = 0;
-			var left = 0;
-			var header = $("tblViewData_${control.tableViewerId}");
-			if (header) {
-				var colTbl = header.select("table.tbvColHeader");
-				if (colTbl && colTbl.length >= $control.positionIdx) {
-					var layout = colTbl[$control.positionIdx].getLayout();
-					var lDiv = $("tblViewHead_${control.tableViewerId}");
-					top = layout.get('top') + layout.get('height') + 2;
-					left = layout.get('left') - lDiv.scrollLeft;
-					if (left + myWidth > document.viewport.getDimensions().width - 4) {
-						left = document.viewport.getDimensions().width - myWidth - 4;
-					}
+		//var win = jQuery('#'+JQryEscape('win_${control.controlID}_div'));	
+		var dialogWidth = 300;
+		//dialogWidth += dialogWidth*10/100;
+		var dialogHeight= $control.height;
+		console.log(dialogHeight);
+		dialogHeight+= dialogHeight*10/100;
+		console.log(dialogHeight);
+		
+		var win = jQuery('#'+JQryEscape('filter_${control.controlID}_div')).dialog({
+			autoOpen: false,
+			width : dialogWidth,
+			//height : dialogHeight,
+			closeOnEscape: false,
+			open: function(event, ui) {
+				jQuery(".ui-dialog-titlebar",jQuery(this).parent()).hide();
+				jQuery(".ui-dialog-titlebar-close",jQuery(this).parent()).hide(); 
+			},			
+			draggable:false,
+			resizable : false,
+			modal: false,
+			cache:false,
 					
-				}
-			}
+		});
+		
+		win.parent().appendTo(jQuery("#jwicform"));		
+		win.dialog('open');	
+		
+		
+		var header = jQuery('#'+JQryEscape('tblViewData_${control.tableViewerId}'));
+		if(header){
+			var column = jQuery(jQuery('table.tbvColHeader',header)[$control.positionIdx]);
+			console.log(column.offset());
+			var offset = column.offset();
+			console.log(win.dialog('option','position'));
+			win.dialog('option','position',[offset.left,offset.top+column.height()])
 			
-			win = new Window({
-				className : "j-combo-content",
-				#* title: "$escape.escapeJavaScript($control.column.title)", *#
-				top : top,
-				left : left,
-				width : myWidth,
-				height: ${control.height},
-				resizable : false,
-				closable : false,
-				minimizable : false,
-				maximizable : false,
-				draggable : false,
-				showEffect: Element.show,
-				#* Add IE6 fix *#
-				#if(!$control.modal && $control.sessionContext.userAgent.isIE() && $control.sessionContext.userAgent.getMajorVersion() < 7)
-					onMove : function() {JWic.controls.Window.adjustIEBlocker("$control.controlID")},
-					onResize : function() {JWic.controls.Window.adjustIEBlocker("$control.controlID")},
-					onMaximize : function() {JWic.controls.Window.adjustIEBlocker("$control.controlID")},
-					onMinimize : function() {JWic.controls.Window.adjustIEBlocker("$control.controlID")},
-				#end
-				id : "$control.controlID",
-				parent : $("jwicform")
-			});
-			win.setContent("win_${control.controlID}_div", false, false);
-			win.setCloseCallback(function () {
-				JWic.fireAction("$control.controlID", "close", "");
-				return true;
-			});
+			console.log(win.dialog('option','position'));
+		}	
 			
-			win._positionIndex = $control.positionIdx;
-			
-			if (top == 0 && left == 0) {
-				win.showCenter(false);
-			} else {
-				win.show(false);
-			}
-			
-			#if(!$control.modal && $control.sessionContext.userAgent.isIE() && $control.sessionContext.userAgent.getMajorVersion() < 7)
-				JWic.controls.Window.adjustIEBlocker("$control.controlID");
-			#end
-		}
-	#end
 	},
 	
 	/**
 	 * Destroy the window if it still exists.
 	 */
 	destroy : function(element) {
-		var win = Windows.getWindow("$control.controlID");
-		if (win && win.destroy) {
-			win.hide();
-			win.destroy();
-		}
+		var win = jQuery('#'+JQryEscape('filter_${control.controlID}_div'));	
+		win.dialog('destroy').remove();
+		
 	}
 }
