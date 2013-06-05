3acd9b8ecb43cddbd3e037981324f86ae8d36d06
Alexandru Bledea <abledea@ABLEDEA01-PC.hq.netapp.com>
2013-06-05 13:39:14 +0300

    log entity list view exception

--- de.xwic.appkit.webbase/src/de/xwic/appkit/webbase/entityviewer/EntityListView.java
+++ de.xwic.appkit.webbase/src/de/xwic/appkit/webbase/entityviewer/EntityListView.java
@@ -658,26 +658,33 @@ public class EntityListView extends ControlContainer implements IEntityProvider 
 	 */
 	@Override
 	public IEntity getEntity() {
-		if (!hasEntity()) {
-			return null;
-		}
-		
 		try {
-			String selection = getEntityKey();
-			
-			if (selection.trim().length() > 0) {
-				DAO dao = DAOSystem.findDAOforEntity(configuration.getEntityClass());
-
-				int id = Integer.parseInt(selection);
-				return dao.getEntity(id);
-			}
-			
-			return null;
+			return getEntityThrowingException();
 		} catch (Exception e) {
+			log.error(e.getMessage(), e);
 			return null;
 		}
 	}
 	
+	/**
+	 * @return
+	 * @throws Exception
+	 */
+	public IEntity getEntityThrowingException() throws Exception {
+		if (!hasEntity()) {
+			return null;
+		}
+
+		String selection = getEntityKey();
+
+		if (selection.trim().length() > 0) {
+			int id = Integer.parseInt(selection);
+			return dao.getEntity(id);
+		}
+
+		return null;
+	}
+
 	/* (non-Javadoc)
 	 * @see de.xwic.appkit.webbase.actions.IEntityProvider#getBaseEntity()
 	 */
