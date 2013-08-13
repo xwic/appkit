da7ba3d97fc211861f2786661c2310f8d65fcba5
Alexandru Bledea <abledea@ABLEDEA01-PC.hq.netapp.com>
2013-08-13 13:52:27 +0300

    more entityutils

--- de.xwic.appkit.core/src/de/xwic/appkit/core/model/util/EntityUtil.java
+++ de.xwic.appkit.core/src/de/xwic/appkit/core/model/util/EntityUtil.java
@@ -5,10 +5,15 @@ package de.xwic.appkit.core.model.util;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import de.xwic.appkit.core.dao.DAO;
 import de.xwic.appkit.core.dao.DAOSystem;
 import de.xwic.appkit.core.dao.IEntity;
+import de.xwic.appkit.core.dao.Limit;
+import de.xwic.appkit.core.dao.impl.hbn.HibernateUtil;
+import de.xwic.appkit.core.model.queries.PropertyQuery;
 import de.xwic.appkit.core.util.CollectionUtil;
 import de.xwic.appkit.core.util.IEvaluator;
 
@@ -31,7 +36,7 @@ public class EntityUtil {
 	 * @param collection
 	 * @return
 	 */
-	public static Set<Integer> getIds(Collection<? extends IEntity> collection) {
+	public static <E extends IEntity> Set<Integer> getIds(Collection<E> collection) {
 		return CollectionUtil.createCollection(collection, ENTITY_ID_EVALUATOR, new HashSet<Integer>());
 	}
 
@@ -41,7 +46,7 @@ public class EntityUtil {
 	 * @return
 	 */
 	public static <E extends IEntity> E getEntity(Class<E> entityClass, Integer id) {
-		return id == null ? null : (E) DAOSystem.findDAOforEntity(entityClass).getEntity(id);
+		return id == null ? null : (E) findDAO(entityClass).getEntity(id);
 	}
 
 	/**
@@ -49,6 +54,52 @@ public class EntityUtil {
 	 * @return
 	 */
 	public static <E extends IEntity> E createEntity(Class<E> entityClass) {
-		return (E) DAOSystem.findDAOforEntity(entityClass).createEntity();
+		return (E) findDAO(entityClass).createEntity();
 	}
+
+	/**
+	 * @param entityClass
+	 * @return
+	 */
+	public static <E extends IEntity> String buildTitle(E entity) {
+		return findDAO(entity.getClass()).buildTitle(entity);
+	}
+
+	/**
+	 * @param entityClass
+	 * @return
+	 */
+	public static <E extends IEntity> DAO findDAO(Class<E> entityClass) {
+		return DAOSystem.findDAOforEntity(entityClass);
+	}
+
+	/**
+	 * @param entity
+	 * @return
+	 */
+	public static <E extends IEntity> E refreshEntity(E entity) {
+		HibernateUtil.currentSession().refresh(entity);
+		return entity;
+	}
+
+	/**
+	 * @param entityClass
+	 * @param limit
+	 * @param query
+	 * @return
+	 */
+	public static <E extends IEntity> List<E> getEntities(Class<E> entityClass, PropertyQuery query) {
+		return getEntities(entityClass, null, query);
+	}
+
+	/**
+	 * @param entityClass
+	 * @param limit
+	 * @param query
+	 * @return
+	 */
+	public static <E extends IEntity> List<E> getEntities(Class<E> entityClass, Limit limit, PropertyQuery query) {
+		return findDAO(entityClass).getEntities(limit, query);
+	}
+
 }
