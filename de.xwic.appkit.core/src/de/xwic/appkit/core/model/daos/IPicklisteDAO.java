1424490bed72c1f32a57fb10ae513d850f60f5c5
Alexandru Bledea <Alexandru.Bledea@netapp.com>
2013-10-21 17:02:36 +0300

    parameterize stuff

--- de.xwic.appkit.core/src/de/xwic/appkit/core/model/daos/IPicklisteDAO.java
+++ de.xwic.appkit.core/src/de/xwic/appkit/core/model/daos/IPicklisteDAO.java
@@ -15,58 +15,58 @@ import de.xwic.appkit.core.model.entities.IPickliste;
  * DAO implementation for the IPickliste entity type.
  * All necessary methods for creating Picklist parts like
  * entries, texts are provided with this DAO.
- * 
+ *
  * @author Florian Lippisch
  */
-public interface IPicklisteDAO extends DAO {
+public interface IPicklisteDAO extends DAO<IPickliste> {
 
 	/**
  	 * Create a new IPickliste instance. <p>
 	 * An update is not necessary, because it is done already
 	 * in this methode.
-	 * 
+	 *
 	 * @param key for the Pickliste
-	 * @param title 
+	 * @param title
 	 * @param beschreibung
 	 * @return new IPickListe instance
 	 */
     public IPickliste create(String key, String title, String beschreibung);
-	
+
 	/**
  	 * Create a new IPicklistEntry instance for the given list. <p>
 	 * An update is not necessary, because it is done already
 	 * in this methode.
-	 * 
+	 *
 	 * @param the list, for which the Entry has to be created for
-	 * @return new IPicklistEntry instance 
+	 * @return new IPicklistEntry instance
 	 */
     public IPicklistEntry createPickListEntry(IPickliste list);
-	
+
     /**
  	 * Create a new IPicklistText instance for the given PickListEntry. <p>
 	 * An update is not necessary, because it is done already
 	 * in this methode.
-     * 
-     * @param the entry, for which the Bezeichnung has to be created for 
-     * @param langID 
+     *
+     * @param the entry, for which the Bezeichnung has to be created for
+     * @param langID
      * @param bezeichnung for the given lang_id to the specific entry
      * @return new IPicklistText instance
      */
     public IPicklistText createBezeichnung(IPicklistEntry entry, String langID, String bezeichnung);
-    
+
     /**
      * Returns the IPicklistText to a specific PicklistEntry and the given language ID. <p>
-     * 
+     *
      * @param entry
      * @param langID
      * @return IPicklistText
      */
     public IPicklistText getPicklistText(IPicklistEntry entry, String langID);
-    
+
     /**
      * Returns all Entries for one given Pickliste. <p>
-     * If nothing is found, the Entitylist is empty. 
-     * 
+     * If nothing is found, the Entitylist is empty.
+     *
      * @param list
      * @return EntityList
      */
@@ -74,35 +74,35 @@ public interface IPicklisteDAO extends DAO {
 
     /**
      * Get all entries to a List by Picklistkey. <p>
-     * 
+     *
      * @param pickListKey as a String
      * @return all Entries to the list
      */
     public EntityList getAllEntriesToList(String pickListKey);
-    	
+
     /**
      * This methode should be used, when you want to search for PickListEntries. <p>
-     * 
+     *
      * The methode getEntityById() does not work in this case, because it holds
      * the PickListe as class identifier. <br>
-     * 
+     *
      * @param id
      * @return IPicklistEntry
      */
     public IPicklistEntry getPickListEntryByID(int id);
-    
+
     /**
      * Returns the picklistText object with the specified id.
      * @param id
      * @return
      */
     public IPicklistText getPickListTextByID(int id);
-    
+
     /**
      * Get a Pickliste from the database by the given key. <p>
-     * 
+     *
      * The keys are public variables of the IPickliste interface.
-     * 
+     *
      * @param key of the Pickliste type
      * @return IPickliste
      */
@@ -110,13 +110,13 @@ public interface IPicklisteDAO extends DAO {
 
     /**
      * Get a PicklistEntry from the database by the given key. <p>
-     * 
+     *
      * @param key of the Pickliste type
      * @return IPicklistEntry
      */
     public IPicklistEntry getPickListEntryByKey(String listKey, String key);
 
-    
+
 	/**
 	 * Drops the cache. <p>
 	 */
@@ -126,12 +126,12 @@ public interface IPicklisteDAO extends DAO {
      * Loads all Picklist, PicklistEntry and PicklistText objects into the cache.
      */
     public void cacheAll();
- 
+
     /**
      * Creates a Picklistentry without update and without picklist relation.
-     * 
+     *
      * @return an unsaved entity!
      */
     public IPicklistEntry createPicklistEntry();
-    
+
 }
--- de.xwic.appkit.core/src/de/xwic/appkit/core/model/daos/impl/MonitoringElementDAO.java
+++ de.xwic.appkit.core/src/de/xwic/appkit/core/model/daos/impl/MonitoringElementDAO.java
@@ -1,6 +1,6 @@
 /*
- * (c) Copyright 2005, 2006 by pol GmbH 
- * 
+ * (c) Copyright 2005, 2006 by pol GmbH
+ *
  * de.xwic.appkit.core.model.daos.impl.MonitoringElementDAO
  * Created on 04.01.2008 by Florian Lippisch
  *
@@ -9,7 +9,6 @@ package de.xwic.appkit.core.model.daos.impl;
 
 import de.xwic.appkit.core.dao.AbstractDAO;
 import de.xwic.appkit.core.dao.DataAccessException;
-import de.xwic.appkit.core.dao.Entity;
 import de.xwic.appkit.core.dao.IEntity;
 import de.xwic.appkit.core.model.daos.IMonitoringElementDAO;
 import de.xwic.appkit.core.model.entities.IMonitoringElement;
@@ -18,27 +17,21 @@ import de.xwic.appkit.core.model.entities.impl.MonitoringElement;
 /**
  * @author Florian Lippisch
  */
-public class MonitoringElementDAO extends AbstractDAO implements IMonitoringElementDAO {
+public class MonitoringElementDAO extends AbstractDAO<IMonitoringElement, MonitoringElement> implements IMonitoringElementDAO {
 
-	/* (non-Javadoc)
-	 * @see de.xwic.appkit.core.dao.AbstractDAO#getEntityImplClass()
+	/**
+	 *
 	 */
-	public Class<? extends Entity> getEntityImplClass() {
-		return MonitoringElement.class;
+	public MonitoringElementDAO() {
+		super(IMonitoringElement.class, MonitoringElement.class);
 	}
 
 	/* (non-Javadoc)
 	 * @see de.xwic.appkit.core.dao.DAO#createEntity()
 	 */
+	@Override
 	public IEntity createEntity() throws DataAccessException {
 		return new MonitoringElement();
-	}
-
-	/* (non-Javadoc)
-	 * @see de.xwic.appkit.core.dao.DAO#getEntityClass()
-	 */
-	public Class<? extends IEntity> getEntityClass() {
-		return IMonitoringElement.class;
 	}
 
 }
