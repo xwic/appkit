c15a5d211576fd6c293160103a6b63e649c2374f
apixandru <Alexandru.Bledea@netapp.com>
2014-01-23 11:33:36 +0200

    use correct classloader

--- de.xwic.appkit.core/src/de/xwic/appkit/core/remote/client/EntityProxyFactory.java
+++ de.xwic.appkit.core/src/de/xwic/appkit/core/remote/client/EntityProxyFactory.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package de.xwic.appkit.core.remote.client;
 
@@ -19,16 +19,30 @@ public class EntityProxyFactory {
 	 * @param eto
 	 * @return
 	 */
-	public static IEntity createEntityProxy(EntityTransferObject eto) {
-		
-		ETOProxyHandler handler = new ETOProxyHandler(eto);
+	public static IEntity createEntityProxy(final EntityTransferObject eto) {
+
+		final ETOProxyHandler handler = new ETOProxyHandler(eto);
 
 		// build list of interfaces
-		Class[] interfaces = eto.getEntityClass().getInterfaces();
-		
-		return (IEntity) Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(), 
-				interfaces, 
+		final Class[] interfaces = eto.getEntityClass().getInterfaces();
+
+		return (IEntity) Proxy.newProxyInstance(getClassLoader(interfaces),
+				interfaces,
 				handler);
 	}
-	
+
+	/**
+	 * @param interfaces
+	 * @return
+	 */
+	private static ClassLoader getClassLoader(final Class... interfaces) {
+		if (interfaces != null && interfaces.length > 0) {
+			return interfaces[0].getClassLoader();
+		}
+//		i think that the classloader that contains IEntity would be a better idea
+//		since etoClass is bound to have IEntity in it, the system classloader isn't
+//		return ClassLoader.getSystemClassLoader();
+		return IEntity.class.getClassLoader();
+	}
+
 }
