f7f5de0170653779bdd2fa63144d900958d06e30
apixandru <Alexandru.Bledea@netapp.com>
2014-05-28 12:13:42 +0300

    more tests

--- de.xwic.appkit.core/src_test/de/xwic/appkit/core/model/queries/resolver/hbn/PropertyQueryTest.java
+++ de.xwic.appkit.core/src_test/de/xwic/appkit/core/model/queries/resolver/hbn/PropertyQueryTest.java
@@ -3,19 +3,26 @@
  */
 package de.xwic.appkit.core.model.queries.resolver.hbn;
 
+import static de.xwic.appkit.core.util.CollectionUtil.cloneToSet;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 
 import de.xwic.appkit.core.dao.EntityQuery;
-import de.xwic.appkit.core.dao.IEntity;
 import de.xwic.appkit.core.dao.IEntityQueryResolver;
 import de.xwic.appkit.core.model.entities.IMitarbeiter;
-import de.xwic.appkit.core.model.entities.IPicklistEntry;
 import de.xwic.appkit.core.model.queries.PropertyQuery;
+import de.xwic.appkit.core.model.queries.QueryElement;
 
 /**
  * @author Alexandru Bledea
@@ -23,18 +30,65 @@ import de.xwic.appkit.core.model.queries.PropertyQuery;
  */
 public class PropertyQueryTest {
 
+	private static final Class<IMitarbeiter> MITARBEITER = IMitarbeiter.class;
+
 	private static final IEntityQueryResolver resolver = new PropertyQueryResolver();
+	private static final String BASIC_QUERY = " FROM de.xwic.appkit.core.model.entities.IMitarbeiter AS obj \n WHERE  obj.deleted = 0 ";
 
 	/**
 	 * 
 	 */
 	@Test
 	public void testSimpleQuery() {
-		final PropertyQuery pq = new PropertyQuery();
-		final String onlyQuery = onlyQuery(IMitarbeiter.class, pq);
-		final String expectedQuery = " FROM de.xwic.appkit.core.model.entities.IMitarbeiter AS obj \n" +
-				" WHERE  obj.deleted = 0  ";
-		assertEquals(expectedQuery, onlyQuery);
+		assertEquals(BASIC_QUERY + " ", onlyQuery(new PropertyQuery()));
+	}
+
+	/**
+	 * 
+	 */
+	@Test
+	public void testInQuery() {
+		{
+			final List<QueryElement> values = new ArrayList<QueryElement>();
+			final PropertyQuery pq = new PropertyQuery();
+			final List<Integer> ids = Arrays.asList(1, 2, 3, 4, 5);
+			pq.addIn("id", ids);
+
+			final String expectedQuery = BASIC_QUERY + "AND (obj.id IN (?,?,?,?,?)) ";
+			assertEquals(expectedQuery, generate(pq, values));
+
+//			test arguments
+			assertSame(1, values.size());
+			final Set<Integer> ints = new HashSet<Integer>();
+			for (final QueryElement integer : values) {
+				ints.addAll((Collection<Integer>) integer.getValue());
+			}
+			assertEquals(cloneToSet(ids), ints);
+		}
+
+		{
+			final List<QueryElement> values = new ArrayList<QueryElement>();
+			final PropertyQuery pq = new PropertyQuery();
+			final List<Integer> ids = new ArrayList<Integer>();
+			for (int i = 0; i < 1001; i++) {
+				ids.add(i);
+			}
+			pq.addIn("id", ids);
+
+			final String thousandQs = StringUtils.join(Collections.nCopies(1000, '?'), ',');
+			final String expectedQuery = BASIC_QUERY + "AND (obj.id IN (" + thousandQs + ") OR obj.id IN (?)) ";
+			assertEquals(expectedQuery, generate(pq, values));
+
+//			test arguments
+			assertSame(2, values.size());
+
+//			test arguments
+			final Set<Integer> ints = new HashSet<Integer>();
+			for (final QueryElement integer : values) {
+				ints.addAll((Collection<Integer>) integer.getValue());
+			}
+			assertEquals(cloneToSet(ids), ints);
+		}
 	}
 
 	/**
@@ -42,7 +96,7 @@ public class PropertyQueryTest {
 	 */
 	@Test (expected = IllegalArgumentException.class)
 	public void testNullQuery() {
-		onlyQuery(IEntity.class, null);
+		onlyQuery(null);
 	}
 
 	/**
@@ -50,18 +104,31 @@ public class PropertyQueryTest {
 	 */
 	@Test (expected = IllegalArgumentException.class)
 	public void testWrongQuery() {
-		onlyQuery(IPicklistEntry.class, new EntityQuery() {
+		onlyQuery(new EntityQuery() {
 		});
 	}
 
 	/**
-	 * @param clasz
+	 * @param MITARBEITER
 	 * @param query
 	 * @return
 	 */
-	@SuppressWarnings ({"rawtypes", "unchecked"})
-	private static String onlyQuery(final Class<? extends Object> clasz, final EntityQuery query) {
-		final List list = new ArrayList();
-		return resolver.generateQuery(clasz, query, false, list, list, list, list);
+	private static String onlyQuery(final EntityQuery query) {
+		return generate(query, new ArrayList<QueryElement>());
 	}
+
+	/**
+	 * @param query
+	 * @param values
+	 * @param customWhereClauses
+	 * @param customFromClauses
+	 * @param customValues
+	 */
+	private static String generate(final EntityQuery query, final List<QueryElement> values) {
+		final List<String> customWhereClauses = new ArrayList<String>();
+		final List<String> customFromClauses = new ArrayList<String>();
+		final List<Object> customValues = new ArrayList<Object>();
+		return resolver.generateQuery(MITARBEITER, query, false, values, customFromClauses, customWhereClauses, customValues);
+	}
+
 }
