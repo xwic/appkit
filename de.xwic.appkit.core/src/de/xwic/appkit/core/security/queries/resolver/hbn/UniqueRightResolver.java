e365d5edcc5b93987b2839e09129e44376afdf9f
Alexandru Bledea <Alexandru.Bledea@netapp.com>
2014-08-19 10:31:23 +0300

    remove empty class

--- de.xwic.appkit.core/src/de/xwic/appkit/core/security/queries/resolver/hbn/UniqueRightResolver.java
+++ de.xwic.appkit.core/src/de/xwic/appkit/core/security/queries/resolver/hbn/UniqueRightResolver.java
@@ -11,7 +11,7 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 
 import de.xwic.appkit.core.dao.EntityQuery;
-import de.xwic.appkit.core.dao.EntityQueryAdapter;
+import de.xwic.appkit.core.dao.IEntityQueryResolver;
 import de.xwic.appkit.core.dao.impl.hbn.HibernateUtil;
 import de.xwic.appkit.core.security.IRight;
 import de.xwic.appkit.core.security.impl.Right;
@@ -22,7 +22,7 @@ import de.xwic.appkit.core.security.queries.UniqueRightQuery;
  *  
  * @author Florian Lippisch
  */
-public class UniqueRightResolver extends EntityQueryAdapter {
+public class UniqueRightResolver implements IEntityQueryResolver {
 
 	/* (non-Javadoc)
 	 * @see de.xwic.appkit.core.dao.IEntityQueryResolver#resolve(de.xwic.appkit.core.dao.EntityQuery)
--- de.xwic.appkit.core/src/de/xwic/appkit/core/security/queries/resolver/hbn/UniqueScopeResolver.java
+++ de.xwic.appkit.core/src/de/xwic/appkit/core/security/queries/resolver/hbn/UniqueScopeResolver.java
@@ -11,7 +11,7 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 
 import de.xwic.appkit.core.dao.EntityQuery;
-import de.xwic.appkit.core.dao.EntityQueryAdapter;
+import de.xwic.appkit.core.dao.IEntityQueryResolver;
 import de.xwic.appkit.core.dao.impl.hbn.HibernateUtil;
 import de.xwic.appkit.core.security.IScope;
 import de.xwic.appkit.core.security.impl.Scope;
@@ -22,7 +22,7 @@ import de.xwic.appkit.core.security.queries.UniqueScopeQuery;
  *  
  * @author Florian Lippisch
  */
-public class UniqueScopeResolver extends EntityQueryAdapter {
+public class UniqueScopeResolver implements IEntityQueryResolver {
 
 	/* (non-Javadoc)
 	 * @see de.xwic.appkit.core.dao.IEntityQueryResolver#resolve(de.xwic.appkit.core.dao.EntityQuery)
