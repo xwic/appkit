5c70ea39b41008a26db09474443c3f18b249d372
Alexandru Bledea <Alexandru.Bledea@netapp.com>
2014-10-14 08:57:19 +0300

    change to functions

--- de.xwic.appkit.core/src/de/xwic/appkit/core/util/Functions.java
+++ de.xwic.appkit.core/src/de/xwic/appkit/core/util/Functions.java
@@ -9,13 +9,13 @@ package de.xwic.appkit.core.util;
  * @author boogie
  * @since  Aug 18, 2014
  */
-public abstract class LazyEval<A, B> implements Function<A, B> {
+public abstract class Functions<A, B> implements Function<A, B> {
 
-	private LazyEval() {
+	private Functions() {
 	}
 	
 	/**
-	 * Returns a new LazyEval that represents calling this and the next eval in sequence
+	 * Returns a new Function that represents calling this and the next eval in sequence
 	 * <pre>
 	 * 		//equivalent to this
 	 * 		final B evaluate = this.evaluate(param);
@@ -29,11 +29,11 @@ public abstract class LazyEval<A, B> implements Function<A, B> {
 	 * @param next - the next evaluator in the chain
 	 * @return a new LazyEval instance representing a sequence call of this and next
 	 */
-	public <C> LazyEval<A, C> andThen(final Function<B, C> next){
-		return new LazyEval<A, C>() {
+	public <C> Functions<A, C> andThen(final Function<B, C> next){
+		return new Functions<A, C>() {
 			@Override
 			public C evaluate(A obj) {
-				final B evaluate = LazyEval.this.evaluate(obj);
+				final B evaluate = Functions.this.evaluate(obj);
 				if(evaluate == null){
 					return null;
 				}
@@ -47,8 +47,8 @@ public abstract class LazyEval<A, B> implements Function<A, B> {
 	 * @param eval
 	 * @return
 	 */
-	public static <A, B> LazyEval<A, B> of(final Function<A, B> eval){
-		return new LazyEval<A, B>() {
+	public static <A, B> Functions<A, B> of(final Function<A, B> eval){
+		return new Functions<A, B>() {
 			@Override
 			public B evaluate(A obj) {
 				return eval.evaluate(obj);
