cfedffe9ba00ca3beb17511d338aed37166956ef
Adrian Ionescu <ionescu@netapp.com>
2015-09-09 17:24:57 +0200

    set the version after remote update

--- de.xwic.appkit.core/src/de/xwic/appkit/core/dao/impl/remote/RemoteDAOProviderAPI.java
+++ de.xwic.appkit.core/src/de/xwic/appkit/core/dao/impl/remote/RemoteDAOProviderAPI.java
@@ -86,6 +86,9 @@ public class RemoteDAOProviderAPI implements DAOProviderAPI {
 			entity.setLastModifiedFrom((String) eto.getPropertyValue("lastModifiedFrom").getValue());
 			entity.setCreatedAt((Date) eto.getPropertyValue("createdAt").getValue());
 			entity.setCreatedFrom((String) eto.getPropertyValue("createdFrom").getValue());
+			if (entity instanceof Entity) {
+				((Entity) entity).setVersion(eto.getEntityVersion());
+			}
 			
 		} catch (Exception e) {
 			throw new DataAccessException("Error updating remote entity '" + entity.type().getName() + "' #" + entity.getId(), e);
