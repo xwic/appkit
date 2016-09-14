4f28981964f425992b2621b9c3a2721f1a5c5bf9
Andrei Pat <andreip@netapp.com>
2016-09-14 17:06:44 +0300

    add traces

--- de.xwic.appkit.core/src/de/xwic/appkit/core/dao/impl/hbn/HibernateDAOProviderAPI.java
+++ de.xwic.appkit.core/src/de/xwic/appkit/core/dao/impl/hbn/HibernateDAOProviderAPI.java
@@ -83,56 +83,79 @@ public class HibernateDAOProviderAPI implements DAOProviderAPI {
     /* (non-Javadoc)
      * @see de.xwic.appkit.core.dao.DAOProvider#getEntity(int)
      */
-    public IEntity getEntity(Class<? extends Object> clazz, int id) throws DataAccessException {
-        
-        return (IEntity) session.get(clazz, new Integer(id));
-        
-    }
+	public IEntity getEntity(Class<? extends Object> clazz, int id) throws DataAccessException {
+		ITraceOperation traceOp = null;
+		if (Trace.isEnabled()) {
+			traceOp = Trace.startOperation(DAO.TRACE_CAT);
+		}
+		try {
+			return (IEntity) session.get(clazz, new Integer(id));
+		} finally {
+			if (traceOp != null) {
+				traceOp.finished();
+			}
+		}
+	}
     
 
-    /* (non-Javadoc)
-     * @see de.xwic.appkit.core.dao.DAOProvider#remove(de.xwic.appkit.core.dao.IEntity)
-     */
-    public void delete(IEntity entity) throws DataAccessException {
-    	//System.out.println("DELETE " + entity + " #" + entity.getId() + " - ver: " + entity.getVersion());
-        session.delete(entity);
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.xwic.appkit.core.dao.DAOProvider#remove(de.xwic.appkit.core.dao.IEntity)
+	 */
+	public void delete(IEntity entity) throws DataAccessException {
+		//System.out.println("DELETE " + entity + " #" + entity.getId() + " - ver: " + entity.getVersion());
+		ITraceOperation traceOp = null;
+		if (Trace.isEnabled()) {
+			traceOp = Trace.startOperation(DAO.TRACE_CAT);
+		}
+		try {
+			session.delete(entity);
+		} finally {
+			if (traceOp != null) {
+				traceOp.finished();
+			}
+		}
+	}
 
     /* (non-Javadoc)
      * @see de.xwic.appkit.core.dao.DAOProvider#update(de.xwic.appkit.core.dao.IEntity)
      */
-    public void update(IEntity entity) throws DataAccessException {
-        
-    	IUser user = DAOSystem.getSecurityManager().getCurrentUser(); 
-    	String userName = "<no-user>";
-    	if (user != null) {
-    		// FLI: always use logon name now, as its the unique identifier.
-    		userName = user.getLogonName(); // user.getName() == null ? user.getLogonName() : user.getName();
-    	}
-    	
-    	
-        //set the common properties before saving the object again or at first time
-        Entity e = (Entity)entity;
-        Date date = new Date();
-        
-        //history is always new, so ignore these objects
-        if (e.getId() == 0 && (!(entity instanceof IHistory))) {
-            //if it is new, set these properties...
-            e.setCreatedAt(date);
-            e.setCreatedFrom(userName);
-        }
-        
-        //more common properties... 
-        e.setLastModifiedAt(date);
-        e.setLastModifiedFrom(userName);
-        
-//        try {
-//        	throw new RuntimeException("saveOrUpdate: " + e.getClass().getName());
-//        } catch (Exception ex) {
-//        	ex.printStackTrace();
-//        }
-       
-        session.saveOrUpdate(e);
+	public void update(IEntity entity) throws DataAccessException {
+		ITraceOperation traceOp = null;
+		if (Trace.isEnabled()) {
+			traceOp = Trace.startOperation(DAO.TRACE_CAT);
+		}
+		try {
+
+			IUser user = DAOSystem.getSecurityManager().getCurrentUser();
+			String userName = "<no-user>";
+			if (user != null) {
+				// FLI: always use logon name now, as its the unique identifier.
+				userName = user.getLogonName(); // user.getName() == null ? user.getLogonName() : user.getName();
+			}
+
+			//set the common properties before saving the object again or at first time
+			Entity e = (Entity) entity;
+			Date date = new Date();
+
+			//history is always new, so ignore these objects
+			if (e.getId() == 0 && (!(entity instanceof IHistory))) {
+				//if it is new, set these properties...
+				e.setCreatedAt(date);
+				e.setCreatedFrom(userName);
+			}
+
+			//more common properties... 
+			e.setLastModifiedAt(date);
+			e.setLastModifiedFrom(userName);
+
+			session.saveOrUpdate(e);
+		} finally {
+			if (traceOp != null) {
+				traceOp.finished();
+			}
+		}
     }
 
     /* (non-Javadoc)
@@ -141,51 +164,6 @@ public class HibernateDAOProviderAPI implements DAOProviderAPI {
     public EntityList getEntities(Class<? extends Object> clazz, Limit limit) {
         return getEntities(clazz, limit, null);
     }
-
-    /*// FLI: Deactivated as this references to the wrong hibernate version!
-    public String getSql(Query query) {
-    	String hql = query.getQueryString();
-    	SessionFactory sf = session.getSessionFactory();
-		SessionFactoryImpl sfimpl = (SessionFactoryImpl) sf; // hack - to get to the actual queries..
-		StringBuffer str = new StringBuffer(256);
-		String s = null;
-		HQLQueryPlan plan = new HQLQueryPlan(hql, false, Collections.EMPTY_MAP, sfimpl);
-
-		QueryTranslator[] translators = plan.getTranslators();
-		for (int i = 0; i < translators.length; i++) {
-			QueryTranslator translator = translators[i];
-			if(translator.isManipulationStatement()) {
-				Iterator<?> iterator = translator.getQuerySpaces().iterator();
-				while ( iterator.hasNext() ) {
-					Object qspace = iterator.next();
-					str.append(qspace);
-					if(iterator.hasNext()) { str.append(", "); } //$NON-NLS-1$
-				}
-
-			} else {
-				Type[] returnTypes = translator.getReturnTypes();
-				str.append(i +": "); //$NON-NLS-1$
-				for (int j = 0; j < returnTypes.length; j++) {
-					Type returnType = returnTypes[j];
-					str.append(returnType.getName());
-					if(j<returnTypes.length-1) { str.append(", "); }							 //$NON-NLS-1$
-				}
-			}
-			str.append("\n-----------------\n"); //$NON-NLS-1$
-			Iterator<?> sqls = translator.collectSqlStrings().iterator();
-			while ( sqls.hasNext() ) {
-				String sql = (String) sqls.next();
-				if (s == null) {
-					s = sql;
-				}
-				str.append(sql);
-				str.append("\n\n");	 //$NON-NLS-1$
-			}
-		}
-		return s;
-
-    }
-    */
     
 	public Query getQuery(Class<? extends Object> clazz, EntityQuery query) {
     	Query q;
@@ -372,72 +350,78 @@ public class HibernateDAOProviderAPI implements DAOProviderAPI {
 		return session;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see de.xwic.appkit.core.dao.DAOProviderAPI#softDelete(de.xwic.appkit.core.dao.IEntity)
 	 */
 	@SuppressWarnings("unchecked")
 	public void softDelete(IEntity entity) throws DataAccessException {
+		ITraceOperation traceOp = null;
+		if (Trace.isEnabled()) {
+			traceOp = Trace.startOperation(DAO.TRACE_CAT);
+		}
+		try {
+			// build a list of possible references
+			List<Property> refList = new ArrayList<Property>();
+			Set<Object> toDelete = new HashSet<Object>();
+			toDelete.add(new EntityKey(entity));
 
-		// build a list of possible references
-		List<Property> refList = new ArrayList<Property>();
-		Set<Object> toDelete = new HashSet<Object>();
-		toDelete.add(new EntityKey(entity));
-		
-		Class<? extends IEntity> type = entity.type();
-		List<Model> models = ConfigurationManager.getSetup().getModels();
-		for (Iterator<Model> itM = models.iterator(); itM.hasNext(); ) {
-			Model model = itM.next();
-			for (Iterator<String> itE = model.getManagedEntities().iterator(); itE.hasNext(); ) {
-				String entityType = itE.next();
-				if (!entityType.equals(IPicklistText.class.getName())) {
-					EntityDescriptor descr = model.getEntityDescriptor(entityType);
-					if (!descr.isHidden()) {
-						for (Iterator<Property> itP = descr.getProperties().values().iterator(); itP.hasNext(); ) {
-							Property prop = itP.next();
-							if (prop.getDescriptor().getPropertyType().equals(type) &&
-									prop.getOnRefDeletion() != Property.IGNORE) {
-								refList.add(prop);
+			Class<? extends IEntity> type = entity.type();
+			List<Model> models = ConfigurationManager.getSetup().getModels();
+			for (Iterator<Model> itM = models.iterator(); itM.hasNext();) {
+				Model model = itM.next();
+				for (Iterator<String> itE = model.getManagedEntities().iterator(); itE.hasNext();) {
+					String entityType = itE.next();
+					if (!entityType.equals(IPicklistText.class.getName())) {
+						EntityDescriptor descr = model.getEntityDescriptor(entityType);
+						if (!descr.isHidden()) {
+							for (Iterator<Property> itP = descr.getProperties().values().iterator(); itP.hasNext();) {
+								Property prop = itP.next();
+								if (prop.getDescriptor().getPropertyType().equals(type) && prop.getOnRefDeletion() != Property.IGNORE) {
+									refList.add(prop);
+								}
 							}
 						}
 					}
 				}
 			}
-		}
-		
-		List<String> badRefList = new ArrayList<String>();
-		
-		Integer intEntityId = new Integer(entity.getId());
-		// iterate through references (and check them)
-		for (Iterator<Property> it = refList.iterator(); it.hasNext(); ) {
-			Property prop = it.next();
-			// must get the right DAO to query
-			DAO dao = DAOSystem.findDAOforEntity(prop.getEntityDescriptor().getClassname());
-			PropertyQuery query = new PropertyQuery();
-			query.addEquals(prop.getName(), intEntityId);
-			
-			List<IEntity> result = dao.getEntities(null, query, false);
-			
-			// handle results
-			for (Iterator<IEntity> itR = result.iterator(); itR.hasNext(); ) {
-				IEntity refE = (IEntity)itR.next();
-				
-				String info = prop.getEntityDescriptor().getClassname() + ";" + prop.getName() + ";" + refE.getId() + ";" + dao.buildTitle(refE);
-				switch (prop.getOnRefDeletion()) {
-					case Property.DENY : {
+
+			List<String> badRefList = new ArrayList<String>();
+
+			Integer intEntityId = new Integer(entity.getId());
+			// iterate through references (and check them)
+			for (Iterator<Property> it = refList.iterator(); it.hasNext();) {
+				Property prop = it.next();
+				// must get the right DAO to query
+				DAO dao = DAOSystem.findDAOforEntity(prop.getEntityDescriptor().getClassname());
+				PropertyQuery query = new PropertyQuery();
+				query.addEquals(prop.getName(), intEntityId);
+
+				List<IEntity> result = dao.getEntities(null, query, false);
+
+				// handle results
+				for (Iterator<IEntity> itR = result.iterator(); itR.hasNext();) {
+					IEntity refE = (IEntity) itR.next();
+
+					String info = prop.getEntityDescriptor().getClassname() + ";" + prop.getName() + ";" + refE.getId() + ";"
+							+ dao.buildTitle(refE);
+					switch (prop.getOnRefDeletion()) {
+					case Property.DENY: {
 						if (!toDelete.contains(new EntityKey(refE))) {
 							// add if the entity is not already in the list of entities to be deleted.
 							badRefList.add(info);
 						}
 						break;
 					}
-					case Property.DELETE : {
-						if (badRefList.size() == 0) {				
+					case Property.DELETE: {
+						if (badRefList.size() == 0) {
 							try {
 								dao.softDelete(refE);
 								toDelete.add(refE);
 							} catch (DataAccessException dae) {
 								if (dae.getMessage().equals("softdelete.hasref")) {
-									badRefList.addAll((Collection<? extends String>)dae.getInfo());
+									badRefList.addAll((Collection<? extends String>) dae.getInfo());
 									badRefList.add(info);
 								} else {
 									throw dae;
@@ -446,8 +430,8 @@ public class HibernateDAOProviderAPI implements DAOProviderAPI {
 						}
 						break;
 					}
-					case Property.CLEAR_REFERENCE : {
-						if (badRefList.size() == 0 && !toDelete.contains(new EntityKey(refE))) {				
+					case Property.CLEAR_REFERENCE: {
+						if (badRefList.size() == 0 && !toDelete.contains(new EntityKey(refE))) {
 							try {
 								PropertyDescriptor propDesc = new PropertyDescriptor(prop.getName(), refE.getClass());
 								Method mWrite = propDesc.getWriteMethod();
@@ -459,29 +443,30 @@ public class HibernateDAOProviderAPI implements DAOProviderAPI {
 						}
 						break;
 					}
-				}
-				
-			}
-			
-		}
-		
-		if (badRefList.size() > 0) {
-			DataAccessException dae = new DataAccessException("softdelete.hasref");
-			dae.setInfo(badRefList);
-			throw dae;
-		}
-		
-		// now modify the entity using its DAO to make sure that
-		// history informations are logged when changing the entity.
-		
-		DAO myDao = DAOSystem.findDAOforEntity(type);
-		Entity e = (Entity)entity;
-		e.setDeleted(true);
-		e.setLastModifiedAt(new Date());
-		myDao.update(e);	
-		
-		
-	}
+					}
 
-	
+				}
+
+			}
+
+			if (badRefList.size() > 0) {
+				DataAccessException dae = new DataAccessException("softdelete.hasref");
+				dae.setInfo(badRefList);
+				throw dae;
+			}
+
+			// now modify the entity using its DAO to make sure that
+			// history informations are logged when changing the entity.
+
+			DAO myDao = DAOSystem.findDAOforEntity(type);
+			Entity e = (Entity) entity;
+			e.setDeleted(true);
+			e.setLastModifiedAt(new Date());
+			myDao.update(e);
+		} finally {
+			if (traceOp != null) {
+				traceOp.finished();
+			}
+		}
+	}	
 }
