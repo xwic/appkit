cef9ba6578d610be8afd92da32bedcf1991ed3da
Florian Lippisch <florian.lippisch@netapp.com>
2017-03-10 20:23:28 +0100

    SubList views now pre-populate the newly created entity with the base entity

--- de.xwic.appkit.webbase/src/de/xwic/appkit/webbase/editors/builders/EListViewBuilder.java
+++ de.xwic.appkit.webbase/src/de/xwic/appkit/webbase/editors/builders/EListViewBuilder.java
@@ -16,27 +16,71 @@
  *******************************************************************************/
 package de.xwic.appkit.webbase.editors.builders;
 
+import java.lang.reflect.InvocationTargetException;
+
 import de.jwic.base.IControl;
 import de.jwic.base.IControlContainer;
 import de.jwic.base.Page;
 import de.xwic.appkit.core.config.ConfigurationException;
 import de.xwic.appkit.core.config.editor.EListView;
 import de.xwic.appkit.core.config.model.Property;
+import de.xwic.appkit.core.dao.DAO;
+import de.xwic.appkit.core.dao.DAOSystem;
 import de.xwic.appkit.core.dao.IEntity;
 import de.xwic.appkit.core.model.queries.PropertyQuery;
+import de.xwic.appkit.webbase.actions.IEntityCreator;
 import de.xwic.appkit.webbase.editors.IBuilderContext;
 import de.xwic.appkit.webbase.editors.mappers.ListViewMapper;
 import de.xwic.appkit.webbase.entityviewer.EntityListView;
 import de.xwic.appkit.webbase.entityviewer.EntityListViewConfiguration;
 
 /**
- * Defines the InputBox builder class.
+ * Creates a ListView, typically in a sub-tab below an editor.
  * 
- * @author Aron Cotrau
  */
 public class EListViewBuilder extends Builder<EListView> {
 
 
+	private class ChildEntityCreator implements IEntityCreator {
+		private DAO<IEntity> dao;
+		private EntityListViewConfiguration listCfg;
+		private String targetPropertyName;
+		
+		/**
+		 * @param dao
+		 * @param listCfg
+		 * @param targetPropertyName
+		 */
+		public ChildEntityCreator(DAO<IEntity> dao, EntityListViewConfiguration listCfg, String targetPropertyName) {
+			super();
+			this.dao = dao;
+			this.listCfg = listCfg;
+			this.targetPropertyName = targetPropertyName;
+		}
+
+		/* (non-Javadoc)
+		 * @see de.xwic.appkit.webbase.actions.IEntityCreator#createEntity()
+		 */
+		@Override
+		public IEntity createEntity() {
+			IEntity newEntity = dao.createEntity();
+			IEntity baseEntity = listCfg.getBaseEntity();
+			if (baseEntity != null) {
+				Property tgProp = dao.getEntityDescriptor().getProperty(targetPropertyName);
+				if (tgProp != null) {
+					try {
+						tgProp.getDescriptor().getWriteMethod().invoke(newEntity, baseEntity);
+					} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+						log.error("Error writing base entity to target property: " + targetPropertyName);
+					}
+				} else {
+					log.warn("No such property: " + targetPropertyName);
+				}
+			}
+			return newEntity;
+		}
+	}
+	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
 	public IControl buildComponents(EListView element, IControlContainer parent, IBuilderContext context) {
@@ -57,6 +101,10 @@ public class EListViewBuilder extends Builder<EListView> {
 			listView.setHeightDecrease(page.getPageSize().getHeight() / 2);
 			context.registerField(null, listView, element, ListViewMapper.MAPPER_ID);
 
+			if (element.getFilterOn() != null && !element.getFilterOn().isEmpty()) {
+				listView.setEntityCreator(new ChildEntityCreator(DAOSystem.findDAOforEntity(element.getType()), entityListViewConfiguration, element.getFilterOn()));
+			}
+			
 			return listView;
 			
 		} catch (ClassNotFoundException e) {
