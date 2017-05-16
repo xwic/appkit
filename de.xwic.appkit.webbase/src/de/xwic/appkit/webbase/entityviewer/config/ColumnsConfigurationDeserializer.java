3b75074f2360d35ae900ceee557960bce883e544
Andrei Pat <andreip@netapp.com>
2017-05-16 18:36:42 +0300

    add the possibility to serialize queries containing IN to user profile

--- de.xwic.appkit.webbase/src/de/xwic/appkit/webbase/entityviewer/config/ColumnsConfigurationDeserializer.java
+++ de.xwic.appkit.webbase/src/de/xwic/appkit/webbase/entityviewer/config/ColumnsConfigurationDeserializer.java
@@ -21,8 +21,10 @@ package de.xwic.appkit.webbase.entityviewer.config;
 
 import java.io.StringReader;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
@@ -250,7 +252,16 @@ public class ColumnsConfigurationDeserializer {
 				log.error(e.getMessage(), e);
 				return null;
 			}
-		} else {
+		} else if (str.startsWith(ColumnsConfigurationSerializer.COLLECTION)) {
+			String val = str.replace(ColumnsConfigurationSerializer.COLLECTION, "");
+			String[] values = val.split(",");
+			List result = new ArrayList<>();
+			for (String v: values) {
+				result.add(deserializeValue(v));
+			}
+			return result;			
+		}
+		else {
 			throw new IllegalArgumentException("Invalid value: " + str);
 		}
 	}
